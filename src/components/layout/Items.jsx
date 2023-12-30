1ef13e4bbe0fffcacc833366c45d6704bfc44e5e
Alejo-Rojas-R <alejandrorojas1152@gmail.com>
2023-12-30 00:36:00 -0500

    feature: adding a landing page and fixing pagination

--- src/components/layout/Items.jsx
+++ src/components/layout/Items.jsx
@@ -2,28 +2,28 @@ import { Item } from './Item'
 import { Container, Spinner, Row } from 'react-bootstrap';
 import { ResultsPagination } from './ResultsPagination';
 
-export const Items = ({ data }) => {
+export const Items = ({ data, loading }) => {
 
-  // Loading spinner
-  if (data.loading === true) {
+    // Loading spinner
+    if (loading === true) {
+        return (
+            <Container className="d-flex align-items-center justify-content-center vh-100">
+                <Spinner animation="border" />
+            </Container>
+        );
+    }
+
     return (
-      <Container className="d-flex align-items-center justify-content-center vh-100">
-        <Spinner animation="border" />
-      </Container>
-    );
-  }
+        <>
+            <Row xs={1} md={2} lg={3} gap={3} className='g-0 item__list'>
+                {data.map((item, index) => (
+                    <Item key={index} item={item} />
+                ))}
+            </Row>
 
-  return (
-    <>
-      <Row xs={1} md={2} lg={3} gap={3} className='g-0 item__list'>
-        {data.data.map((item, index) => (
-          <Item key={index} item={item} />
-        ))}
-      </Row>
-
-      {(data.data[0].total > 9) &&
-        <ResultsPagination total={data.data[0].total} />
-      }
-    </>
-  )
+            {/*(data[0].total > 9) &&
+                <ResultsPagination total={data.data[0].total} />
+                */}
+        </>
+    )
 }
--- src/routes/Routing.jsx
+++ src/routes/Routing.jsx
@@ -1,25 +1,26 @@
 import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
 import { ItemPage } from '../components/pages/ItemPage';
-import { SearchPage } from '../components/pages/SearchPage';
+import { ResultsPage } from '../components/pages/ResultsPage';
+import { HomePage } from '../components/pages/HomePage';
 import { Footer } from '../components/layout/Footer';
-import { Header } from '../components/layout/Header';
 import { LoginPage } from '../components/pages/LoginPage';
 import { RegisterPage } from '../components/pages/RegisterPage';
 import { AccountPage } from '../components/pages/AccountPage';
 import { useSelector } from 'react-redux';
+import { NavBar } from '../components/layout/NavBar';
 
 export const Routing = () => {
     const currentUser = useSelector(state => state.user);
 
     return (
         <BrowserRouter>
-            <Header />
+            <NavBar />
             <div className='body'>
                 <Routes>
-                    <Route path='/' element={<SearchPage />} />
+                    <Route path='/' element={<HomePage />} />
                     <Route path='/home' element={<Navigate to='/' />} />
                     <Route path='/product/:id' element={<ItemPage />} />
-                    <Route path='/search' element={<SearchPage />} />
+                    <Route path='/search' element={<ResultsPage />} />
 
                     <Route path='/login' element={<LoginPage />} />
                     <Route path='/register' element={<RegisterPage />} />
