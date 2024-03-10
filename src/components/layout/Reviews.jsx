cee5e3d888aceabea7bdbb2d311b668227f7ceea
Alejo-Rojas-R <alejandrorojas1152@gmail.com>
2024-03-10 15:12:40 -0500

    env variable

--- src/components/layout/Reviews.jsx
+++ src/components/layout/Reviews.jsx
@@ -7,81 +7,83 @@ import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 
 export const Reviews = ({ reviews }) => {
-    const [errorMessage, setErrorMessage] = useState('')
-    const { response, loading, fetchData, error } = useFetch();
-    const currentUser = useSelector(state => state.user);
-    const { id } = useParams();
-    const { formData, handleChange } = useForm({
-        description: '',
-        score: '',
-        date: Date.now(),
-        idProduct: id,
-    });
+  const [errorMessage, setErrorMessage] = useState('')
+  const { response, loading, fetchData, error } = useFetch();
+  const currentUser = useSelector(state => state.user);
+  const { id } = useParams();
+  const { formData, handleChange } = useForm({
+    description: '',
+    score: '',
+    date: Date.now(),
+    idProduct: id,
+  });
 
-    const [rating, setRating] = useState(0); // State to store user's rating
+  const [rating, setRating] = useState(0); // State to store user's rating
 
-    const handleRatingChange = (newRating) => {
-        setRating(newRating);
-        handleChange({ target: { name: 'score', value: newRating } });
-    };
+  const handleRatingChange = (newRating) => {
+    setRating(newRating);
+    handleChange({ target: { name: 'score', value: newRating } });
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        fetchData('reviews', 'POST', formData, { headers: { 'Authorization': `Bearer ${currentUser.token}` } });
-    };
+    fetchData('reviews', 'POST', formData, { headers: { 'Authorization': `Bearer ${currentUser.token}` } });
+  };
 
-    // Loading spinner
-    if (loading === true) {
-        return (
-            <Container className='d-flex align-items-center justify-content-center vh-100'>
-                <Spinner animation='border' />
-            </Container>
-        );
-    }
-
+  // Loading spinner
+  if (loading === true) {
     return (
-        <>
-            {currentUser &&
-                <Form onSubmit={handleSubmit} className='d-flex flex-column gap-3 justify-content-center p-4'>
-                    <Form.Group controlId='description'>
-                        <Form.Label>Comment</Form.Label>
-                        <Form.Control as='textarea' type='text' name='description' value={formData.description} onChange={handleChange} required />
-                    </Form.Group>
+      <Container className='d-flex align-items-center justify-content-center vh-100'>
+        <Spinner animation='border' />
+      </Container>
+    );
+  }
 
-                    <Form.Group controlId='score'>
-                        <Form.Label className='pe-2'>Score: </Form.Label>
-                        <RatingStars rating={rating} readonly={false} onRatingChange={handleRatingChange} />
-                    </Form.Group>
+  return (
+    <>
+      <Container className='px-4'>
+        {
+          reviews.length === 0 ?
+            <div>
+              No reviews have been left yet for this item.
+            </div>
+            :
+            reviews.map((review, key) => (
+              <>
+                <Container key={key}>
+                  <div>User said:</div>
+                  <div>{review.description}</div>
+                  <RatingStars rating={review.score} readonly={true} showScore={false} />
+                </Container>
+                <hr />
+              </>
 
-                    <Container className='d-flex justify-content-between p-0 gap-2 align-items-center'>
-                        <Button className='rounded-pill' variant='outline-info' type='submit'>
-                            Post
-                        </Button>
-                        {loading && <Spinner size='sm' animation='border' variant='info' />}
-                    </Container>
-                    <Form.Text className='danger' muted>{errorMessage}</Form.Text>
-                </Form>
-            }
-            <Container className='py-4'>
-                {reviews.length === 0 ?
-                    <div>
-                        No reviews have been left for this item.
-                    </div>
-                    :
-                    reviews.map((review, key) => (
-                        <>
-                            <Container key={key}>
-                                <div>User said:</div>
-                                <div>{review.description}</div>
-                                <RatingStars rating={review.score} readonly={true} showScore={false} />
-                            </Container>
-                            <hr />
-                        </>
+            ))
+        }
+      </Container>
+      {currentUser &&
+        <Form onSubmit={handleSubmit} className='d-flex flex-column gap-3 justify-content-center px-4 py-2'>
+          <Form.Group controlId='description'>
+            <Form.Label>Leave a review for this item!</Form.Label>
+            <Form.Control as='textarea' type='text' name='description' value={formData.description} onChange={handleChange} required />
+          </Form.Group>
 
-                    ))
-                }
-            </Container>
-        </>
-    )
+          <Form.Group controlId='score'>
+            <Form.Label className='pe-2'>Score: </Form.Label>
+            <RatingStars rating={rating} readonly={false} onRatingChange={handleRatingChange} />
+          </Form.Group>
+
+          <Container className='d-flex justify-content-between p-0 gap-2 align-items-center'>
+            <Button className='rounded-pill px-4' variant='outline-info' type='submit'>
+              Post
+            </Button>
+            {loading && <Spinner size='sm' animation='border' variant='info' />}
+          </Container>
+          <Form.Text className='danger' muted>{errorMessage}</Form.Text>
+        </Form>
+      }
+
+    </>
+  )
 }
--- src/components/pages/ItemPage.jsx
+++ src/components/pages/ItemPage.jsx
@@ -9,84 +9,84 @@ import { setCart } from '../../redux/cartSlice';
 import { Reviews } from '../layout/Reviews';
 
 export const ItemPage = () => {
-    const { id } = useParams();
+  const { id } = useParams();
 
-    const { response, loading, fetchData } = useFetch();
+  const { response, loading, fetchData } = useFetch();
 
-    const { pathname } = useLocation();
+  const { pathname } = useLocation();
 
-    const data = response?.data;
-    const dispatch = useDispatch();
+  const data = response?.data;
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        fetchData(`products/${id}`);
-    }, [pathname]);
+  useEffect(() => {
+    fetchData(`products/${id}`);
+  }, [pathname]);
 
-    // Loading spinner
-    if (loading === true) {
-        return (
-            <Container className='d-flex align-items-center justify-content-center vh-100'>
-                <Spinner animation='border' />
-            </Container>
-        );
-    }
-
-    if (!data) {
-        return (
-            <Container className='d-flex align-items-center justify-content-center mt-5'>
-                No results
-            </Container>
-        );
-    }
-
-    const formatUSD = Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-    });
-
-    const handleAddToCart = (e) => {
-        e.preventDefault();
-
-        const cart = JSON.parse(localStorage.getItem('cart')) ?? [];
-        const items = JSON.stringify([...cart,
-        {
-            'id': `item_${data.id}`,
-            'title': data.name,
-            'price': data.price,
-            'image': data.imageUrl,
-        }
-        ]);
-
-        localStorage.setItem('cart', items);
-        dispatch(setCart());
-    }
-
+  // Loading spinner
+  if (loading === true) {
     return (
-        <Container>
-            <Row className='py-5'>
-                <Col md={4}>
-                    <ImagesCarousel images={data.imageUrl} />
-                </Col>
-                <Col md={8} >
-                    <Container className='rounded bg-white'>
-                        <h1 className='mb-2'>{data.name}</h1>
-                        <div className='d-flex align-items-center'>
-                            <h3 className='me-2' data-price={data.price}>{formatUSD.format(data.price)}</h3>
-                        </div>
-                        <hr />
-                        <Row className='py-3'>
-                            <p className='mb-2' style={{ whiteSpace: 'pre-line' }}>{data.description}</p>
-                        </Row>
-                        <Button onClick={handleAddToCart} variant='outline-dark' className='rounded-pill'>Add to Cart <i className='bi bi-cart-plus'></i></Button>
-                    </Container>
-                </Col>
+      <Container className='d-flex align-items-center justify-content-center vh-100'>
+        <Spinner animation='border' />
+      </Container>
+    );
+  }
+
+  if (!data) {
+    return (
+      <Container className='d-flex align-items-center justify-content-center mt-5'>
+        No results
+      </Container>
+    );
+  }
+
+  const formatUSD = Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
+  const handleAddToCart = (e) => {
+    e.preventDefault();
+
+    const cart = JSON.parse(localStorage.getItem('cart')) ?? [];
+    const items = JSON.stringify([...cart,
+    {
+      'id': `item_${data.id}`,
+      'title': data.name,
+      'price': data.price,
+      'image': data.imageUrl,
+    }
+    ]);
+
+    localStorage.setItem('cart', items);
+    dispatch(setCart());
+  }
+
+  return (
+    <Container>
+      <Row className='pt-5 pb-3'>
+        <Col md={4}>
+          <ImagesCarousel images={data.imageUrl} />
+        </Col>
+        <Col md={8} >
+          <Container className='rounded bg-white'>
+            <h1 className='mb-2'>{data.name}</h1>
+            <div className='d-flex align-items-center'>
+              <h3 className='me-2' data-price={data.price}>{formatUSD.format(data.price)}</h3>
+            </div>
+            <hr />
+            <Row className='py-3'>
+              <p className='mb-2' style={{ whiteSpace: 'pre-line' }}>{data.description}</p>
             </Row>
-            <Row className='mt-2'>
-                <Reviews reviews={data.review} />
-            </Row>
-            <Row className='mt-2'>
-                <CategoryPreview category={data.category.id} customTitle='Related products' />
-            </Row>
-        </Container>
-    )
+            <Button onClick={handleAddToCart} variant='outline-dark' className='rounded-pill'>Add to Cart <i className='bi bi-cart-plus'></i></Button>
+          </Container>
+        </Col>
+      </Row>
+      <Row className='mt-2'>
+        <Reviews reviews={data.review} />
+      </Row>
+      <Row className='mt-2'>
+        <CategoryPreview category={data.category.id} customTitle='Related products' />
+      </Row>
+    </Container>
+  )
 }
